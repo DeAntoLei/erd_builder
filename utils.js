46d7a72aa3d18e2bdecfd555d185faf6618c0ac7
DeAntoLei <dai17104@uom.edu.gr>
2021-03-31 18:53:04 +0300

    Nav dropdown submenu change

--- utils.js
+++ utils.js
@@ -110,3 +110,16 @@ function enableNavItems() {
   document.getElementById('nav_copy').classList.remove('disabled');
   document.getElementById('nav_dpl').classList.remove('disabled');
 }
+
+$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+  }
+  var $subMenu = $(this).next(".dropdown-menu");
+  $subMenu.toggleClass('show');
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $('.dropdown-submenu .show').removeClass("show");
+  });
+  
+  return false;
+});
